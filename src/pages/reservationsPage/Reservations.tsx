4cc3d81fec4cadb942391f5d2aadd8b388c9d767
RytasV <rytis.valatkevicius@mif.stud.vu.lt>
2025-12-13 12:37:31 +0200

    Add cosmetic changes to calendar

--- src/pages/reservationsPage/Reservations.tsx
+++ src/pages/reservationsPage/Reservations.tsx
@@ -1,36 +1,39 @@
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
 import { useState } from "react";
 import {Box, Typography, Stack} from "@mui/material";
 import interactionPlugin from "@fullcalendar/interaction";
 import { useNavigate } from "react-router-dom";
 
 import "./Reservations.css";
+import { time } from "console";
 
 export default function Reservations() {
   const [selectedEvent, setSelectedEvent] = useState<any>(null);
   const navigate = useNavigate();
 
   const events = [
-    { id: "1", title: "14:00-14:30 Staff 1", date: "2025-12-08", color: "#a2f5d0" },
-    { id: "2", title: "14:00-14:30 Staff 2", date: "2025-12-08", color: "#f5e6a2" },
-    { id: "3", title: "14:00-14:30 Staff 3", date: "2025-12-08", color: "#7fd97a" },
-    { id: "4", title: "14:30-15:00 Staff 1", date: "2025-12-08", color: "#a2f5d0" },
-    { id: "5", title: "14:30-15:00 Staff 2", date: "2025-12-08", color: "#f5e6a2" },
-    { id: "6", title: "15:00-15:30 Staff 1", date: "2025-12-08", color: "#a2f5d0" },
-    { id: "7", title: "15:30-16:00 Staff 1", date: "2025-12-08", color: "#a2f5d0" }
+    { id: "1", title: "Staff 1", start: "2025-12-13T14:00:00", end: "2025-12-13T14:30:00", backgroundColor: "#a2f5d0" },
+    { id: "2", title: "Staff 2", start: "2025-12-13T14:00:00", end: "2025-12-13T14:30:00", backgroundColor: "#f5e6a2" },
+    { id: "3", title: "Staff 3", start: "2025-12-13T14:00:00", end: "2025-12-13T14:30:00", backgroundColor: "#7fd97a" },
+    { id: "4", title: "Staff 1", start: "2025-12-13T14:30:00", end: "2025-12-13T15:00:00", backgroundColor: "#a2f5d0" },
+    { id: "5", title: "Staff 2", start: "2025-12-13T14:30:00", end: "2025-12-13T15:00:00", backgroundColor: "#f5e6a2" },
+    { id: "6", title: "Staff 1", start: "2025-12-13T15:00:00", end: "2025-12-13T15:30:00", backgroundColor: "#a2f5d0" },
+    { id: "7", title: "Staff 1", start: "2025-12-13T15:30:00", end: "2025-12-13T16:00:00", backgroundColor: "#a2f5d0" }
   ];
 
   function renderEvent(eventInfo: any) {
     return (
       <Box
         sx={{
-          backgroundColor: eventInfo.event.extendedProps.color,
-          borderRadius: "20px",
+          backgroundColor: eventInfo.event.backgroundColor,
+          borderRadius: "10px",
           padding: "8px 5px",
           fontSize: "12px",
           color: "#333",
-          display: "inline-block"
+          display: "inline-block",
+          width: "100%",
         }}
       >
         {eventInfo.event.title}
@@ -62,21 +65,33 @@ export default function Reservations() {
         }}
       >
         <FullCalendar
-          plugins={[dayGridPlugin, interactionPlugin]}
-          initialView="dayGridMonth"
+          plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+          initialView="timeGridDay"
           events={events}
           eventContent={renderEvent}
           eventClick={onEventClick}
           firstDay={1}
           contentHeight="auto"
+          allDaySlot={false}
           headerToolbar={{
             left: 'prev title next',
             center: '',
-            right: 'dayGridDay,dayGridWeek,dayGridMonth addAppointmentButton'
+            right: 'timeGridDay,dayGridWeek,dayGridMonth,addAppointmentButton'
             
           }}
           titleFormat={{ year: 'numeric', month: 'long', day: 'numeric' }}
           dayHeaderFormat={{ weekday: 'short' }}
+          slotDuration="00:15:00"
+          slotLabelFormat={{
+            hour: '2-digit',
+            minute: '2-digit',
+            hour12: false
+          }}
+          eventTimeFormat={{
+            hour: '2-digit',
+            minute: '2-digit',
+            hour12: false
+          }}
           customButtons={{
             addAppointmentButton: {
               text: 'Add Appointment',
