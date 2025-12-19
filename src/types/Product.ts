5bd94a7676133e1a091b4d3c9daafdbedb988c73
Karolis814 <karolis.palubinskas@mif.stud.vu.lt>
2025-12-19 04:53:21 +0200

    Update Product.ts

--- src/types/Product.ts
+++ src/types/Product.ts
@@ -1,3 +1,46 @@
+export type ProductType = "ITEM" | "SERVICE";
+
+export type ProductCreateDto = {
+  registrationNumber: string;
+  productType: string;
+  name: string;
+  description: string;
+  basePrice: number;
+  durationMinutes?: number | null;
+  taxCode: string;
+  status: boolean;
+};
+
+export type ProductUpdateDto = {
+  name: string;
+  description: string;
+  basePrice: number;
+  durationMinutes?: number | null;
+  taxCode: string;
+  status: boolean;
+};
+
+export type ListParams = {
+  registrationNumber: string; 
+  type?: string;
+  search?: string;
+  page?: number;
+  limit?: number;
+};
+
+export type ProductDtoDetails = {
+    productId: string;
+    registrationNumber: string;
+    type: string; 
+    name: string;
+    description: string;
+    basePrice: number;
+    durationMinutes?: number | null;
+    taxCode: string;
+    status: boolean;
+  };
+
+
 export interface Product {
   productId: string;              // Guid from backend
   registrationNumber: string;
